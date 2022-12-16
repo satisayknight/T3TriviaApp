dc18da61f9611ac6aca5f8cec86b8e1e29c202c2
the-mighty-fall <vvarfare@gmail.com>
2022-12-16 14:19:49 -0600

    Added the session token to the api on window load

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 
-const SESSION_TOKEN = "";
+let SESSION_TOKEN = "";
 
 
-// let test2 = document.getElementById("test1");
-// test2.addEventListener("click", getQuizData);
+let test2 = document.getElementById("test1");
+test2.addEventListener("click", getQuizData);
 
+window.addEventListener("load", getSessionToken);
 
 function getQuizData() {
-  // session token?
-  //https://opentdb.com/api_token.php?command=request
-  // reset token
 
   let category = 18; // 9-32
   let difficulty = "easy"; // medium  hard
   let typeOfQuestions = "multiple"; // or "boolean"
   let numberOfQuestions = 10; // 1 - 50
 
-  const api_url = `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${difficulty}&type=${typeOfQuestions}`;
+  const api_url = `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${difficulty}&type=${typeOfQuestions}&token=${SESSION_TOKEN}`;
+
+  console.log("Session Token: " + SESSION_TOKEN);
+
 
   fetch(api_url)
     .then((res) => res.json())
@@ -29,4 +30,16 @@ function getQuizData() {
 }
 
 
+function getSessionToken(){
+    const sessionUrl = 'https://opentdb.com/api_token.php?command=request';
 
+    fetch(sessionUrl)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      SESSION_TOKEN = json['token'];
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
