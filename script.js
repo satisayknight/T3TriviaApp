65216fc21c6cab7c3a0dd7703134db211373cc8c
the-mighty-fall <vvarfare@gmail.com>
2022-12-16 14:23:50 -0600

    Reverted index.html changes

--- script.js
+++ script.js
@@ -1,45 +1,54 @@
-
 let SESSION_TOKEN = "";
 
+// let Button_Test = document.getElementById("click-test");
+// Button_Test.addEventListener("click", getQuizData);
 
-let test2 = document.getElementById("test1");
-test2.addEventListener("click", getQuizData);
-
+// eventually we need to store the token locally
+// and only call this if needed.
 window.addEventListener("load", getSessionToken);
 
-function getQuizData() {
-
-  let category = 18; // 9-32
-  let difficulty = "easy"; // medium  hard
-  let typeOfQuestions = "multiple"; // or "boolean"
-  let numberOfQuestions = 10; // 1 - 50
-
+/**
+ *
+ * @param {number} category - a number from 9-32 indicates which category
+ * @param {number} numberOfQuestions - a number indicates how many questions are returned range(1-50)
+ * @param {string} difficulty - a string on diffuculty of questions "easy","medium","hard"
+ * @param {string} typeOfQuestions - a string either boolean for true/fale or multiple choice.
+ */
+function getQuizData(
+  category = 9,
+  numberOfQuestions = 10,
+  difficulty = "easy",
+  typeOfQuestions = "multiple"
+) {
   const api_url = `https://opentdb.com/api.php?amount=${numberOfQuestions}&category=${category}&difficulty=${difficulty}&type=${typeOfQuestions}&token=${SESSION_TOKEN}`;
 
   console.log("Session Token: " + SESSION_TOKEN);
 
-
   fetch(api_url)
     .then((res) => res.json())
     .then((json) => {
-      console.log(json['results']);
+      console.log(json["results"]);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
+/**
+ * Grabs a session token from the opentdb api
+ * ensures that we do not get the same questions if
+ * if querying from the same token
+ */
+function getSessionToken() {
+  const sessionUrl = "https://opentdb.com/api_token.php?command=request";
 
-function getSessionToken(){
-    const sessionUrl = 'https://opentdb.com/api_token.php?command=request';
-
-    fetch(sessionUrl)
+  fetch(sessionUrl)
     .then((res) => res.json())
     .then((json) => {
       console.log(json);
-      SESSION_TOKEN = json['token'];
+      SESSION_TOKEN = json["token"];
     })
     .catch((error) => {
       console.log(error);
     });
-};
+}
